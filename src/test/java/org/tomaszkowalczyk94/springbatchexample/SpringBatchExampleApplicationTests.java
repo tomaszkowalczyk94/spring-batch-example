a229938cceab3069c1bf7067f33965f7923e3d7e
test <test123>
2020-02-04 19:51:02 +0100

    Initial commit

---
+++ src/test/java/org/tomaszkowalczyk94/springbatchexample/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tomaszkowalczyk94.springbatchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
